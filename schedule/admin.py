134fd87527d84c23c3d25043457c436e95f543ba
Marik28 <markel20002000@gmail.com>
2021-01-05 10:58:13 +0600

    added actions to admin panel

--- schedule/admin.py
+++ schedule/admin.py
@@ -7,9 +7,30 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ('time', '__str__', 'day', 'teacher', 'parity')
     list_display_links = ('__str__',)
     list_filter = ('day', 'parity', 'teacher',)
+    actions = ['archive', 'unarchive']
 
-    def archive(self):
-        pass
+    def archive(self, request, queryset):
+        """Заархивировать пары"""
+        row_update = queryset.update(archived=False)
+        if row_update == '1':
+            msg = 'Была заархивирована 1 пара'
+        else:
+            msg = f'Были заархивированы {row_update} пар(/ы)'
+        self.message_user(request, msg)
+
+    def unarchive(self, request, queryset):
+        """Разархивировать пары"""
+        row_update = queryset.update(archived=True)
+        if row_update == '1':
+            msg = 'Была разархивирована 1 пара'
+        else:
+            msg = f'Были разархивированы {row_update} пар(/ы)'
+        self.message_user(request, msg)
+
+    archive.short_description = 'Заархивировать выбранные пары'
+    archive.allowed_permission = ('change',)
+    unarchive.short_description = 'Разархивировать выбранные пары'
+    unarchive.allowed_permission = ('change',)
 
 
 admin.site.register(Subject)

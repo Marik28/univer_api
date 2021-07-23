58ea272a5056dc8d6ce6c9ded9228ddc365a30ac
mark <markel20002000@gmail.com>
2021-07-23 23:14:56 +0600

    изменил апи для списка предметов, убрав вьюху SubjectFilterAPIView

--- subjects/services.py
+++ subjects/services.py
@@ -5,7 +5,10 @@ from schedule.models import Subject
 
 
 def filter_subjects(query: QueryDict):
-    subjects = Subject.objects.filter(
-        Q(name__icontains=query.get("q"))
-    )
+    if len(query) > 0:
+        subjects = Subject.objects.filter(
+            Q(name__icontains=query.get("q"))
+        )
+    else:
+        subjects = Subject.objects.all()
     return subjects
--- subjects/views.py
+++ subjects/views.py
@@ -5,29 +5,20 @@ from rest_framework.response import Response
 
 
 from schedule.models import Subject
-from schedule.serializers import SubjectSerializer
 from subjects.serializers import SubjectDetailSerializer
 from subjects.services import filter_subjects
 
 
 class SubjectListView(views.APIView):
     def get(self, request: Request):
-        subjs = Subject.objects.all()
-        serializer = SubjectDetailSerializer(subjs, many=True)
+        subjects = filter_subjects(request.GET)
+        serializer = SubjectDetailSerializer(subjects, many=True)
         return Response(serializer.data, status=200)
 
 
-class SubjectAPIView(views.APIView):
+class SubjectDetailAPIView(views.APIView):
     """View для отображения информации о конкретном предмете"""
     def get(self, request: Request, pk):
         subj = get_object_or_404(Subject, pk=pk)
         serializer = SubjectDetailSerializer(subj, many=False)
-        return Response(serializer.data, status=200)
-
-
-class SubjectFilterAPIView(views.APIView):
-    """"""
-    def get(self, request: Request):
-        subjects = filter_subjects(request.GET)
-        serializer = SubjectDetailSerializer(subjects, many=True)
         return Response(serializer.data, status=200)

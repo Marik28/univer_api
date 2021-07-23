cecbc4e3626f74aa5972315acd052ca6638a2ae6
mark <markel20002000@gmail.com>
2021-07-23 22:15:34 +0600

    добавил verbose_name для модели Group

--- groups/models.py
+++ groups/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Group(models.Model):
-
     name = models.CharField(max_length=15, verbose_name='Название группы')
 
     class Meta:
         ordering = ['name']
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'

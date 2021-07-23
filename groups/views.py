f30de2895dca10ac1572dc3b71945fe7a4cca07d
mark <markel20002000@gmail.com>
2021-07-23 22:16:12 +0600

    добавил вьюху для просмотра списка групп

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import GroupSerializer
+from .services import get_group_list
+
+
+class GroupListAPIView(views.APIView):
+    def get(self, request: Request):
+        groups = get_group_list()
+        serializer = GroupSerializer(groups, many=True)
+        return Response(serializer.data)

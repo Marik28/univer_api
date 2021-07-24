4655c53f527585b5ac748034d0b288cea87c6c5e
mark <markel20002000@gmail.com>
2021-07-24 14:24:46 +0600

    сделал админку удобнее

--- subjects/admin.py
+++ subjects/admin.py
@@ -6,8 +6,8 @@ from .models import Subject, SubjectName
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
+    list_display = ("name", "group", "subgroup")
+    search_fields = ("name", "group")
     inlines = [LessonInline]
 
 

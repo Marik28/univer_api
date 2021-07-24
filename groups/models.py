362cd1e9271295d5e214a57ba70794b8685da0c4
mark <markel20002000@gmail.com>
2021-07-24 11:43:14 +0600

    добавил поле curator для модели group

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from teachers.models import Teacher
+
 
 class Group(models.Model):
     name = models.CharField(max_length=15, verbose_name='Название группы')
+    curator = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         ordering = ['name']
